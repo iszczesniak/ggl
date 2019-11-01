9ce0163482559b818d987439ad60205046bf2267
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-01 22:32:55 +0100

    minimal_digraph

---
+++ interface/minimal_digraph.cc
@@ -0,0 +1,20 @@
+#include "minimal_digraph.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+template <std::size_t N>
+void
+print(const minimal_digraph<N> &g)
+{
+  for(const auto &v: vertexes(g))
+    for(const auto &e: edges(v))
+      std::cout << source(e) << " -> " << target(e) << std::endl;
+}
+
+int
+main()
+{
+  minimal_digraph<2> g = {{true, false}, {false, true}};
+}
--- interface/minimal_digraph.hpp
+++ interface/minimal_digraph.hpp
@@ -2,14 +2,14 @@
 #define MINIMAL_DIGRAPH_HPP
 
 #include <array>
-#include <vector>
 
 // This is a minimal digraph.  It's minimal, because it has no
 // properties associated with a vertex, and with an edge.  We only
 // know if an edge exists.
 template <std::size_t N>
-using minimal_digraph = std::vector<std::array<bool, N>>[N];
+using minimal_digraph = std::array<std::array<bool, N>, N>;
 
+/*
 template <std::size_t N>
 // We need the "auto &" return type (and not "auto") to prevent the
 // array decay into a pointer.
@@ -25,5 +25,6 @@ edges(const std::array<bool, N>> &v)
 {
   return v;
 }
+*/
 
 #endif /* MINIMAL_DIGRAPH_HPP */
