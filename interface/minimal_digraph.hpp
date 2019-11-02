251996514dc463db236ce2ec40b08c8b039af6c7
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-02 21:18:28 +0100

    minimal_digraph

--- interface/minimal_digraph.hpp
+++ interface/minimal_digraph.hpp
@@ -2,28 +2,40 @@
 #define MINIMAL_DIGRAPH_HPP
 
 #include <array>
+#include <utility>
 
 // This is a minimal digraph.  It's minimal, because it has no
 // properties associated with a vertex, and with an edge.  We only
 // know if an edge exists.
 //
 // We store the graph information in a matrix with boolean values,
-// because the graph can be dense.
+// because the graph can be dense, and because it's not a
+// multidigraph.  The matrix is an array of arrays.  We can consider
+// the nested array as the vertex object.
 //
-// In such a graph we need a way to refer to a vertex, and it's going
-// to be an unsigned integer.
+// In such a graph there is no object that describes a vertex, but we
+// need something that uniquelly describes a vertex, i.e., the vertex
+// descriptor.  An unsigned integer is the vertex descriptor.
+//
+// There is also no object that describes an edge, but we need
+// something that uniquelly describes an edge, i.e., the edge
+// descriptor.  A pair of vertex descriptors is the edge descriptor.
 
 template <std::size_t N>
 using minimal_digraph = std::array<std::array<bool, N>, N>;
 
-
+// Const vertex iterator
 template <std::size_t N>
-// We need the "auto &" return type (and not "auto") to prevent the
-// array decay into a pointer.
-auto &
-vertexes(const minimal_digraph<N> &g)
+using const_vertex_iterator =
+  std::pair<minimal_digraph<N>::size_type,
+            minimal_digraph<N>::const_iterator>;
+
+// Return a range of vertex iterators.
+template <std::size_t N>
+auto
+vertexes(minimal_digraph<N> &g)
 {
-  return g;
+  return make_iterator_range(cost_vertex_iterator<N>(0, g.begin());
 }
 
 template <typename N>
@@ -32,6 +44,5 @@ edges(const std::array<bool, N>> &v)
 {
   return v;
 }
-*/
 
 #endif /* MINIMAL_DIGRAPH_HPP */
