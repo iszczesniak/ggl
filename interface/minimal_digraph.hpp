555c4ec7eeaf72f0d1cc75e029b7c8c024b12b8b
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-02 09:03:43 +0100

    digraph

--- interface/minimal_digraph.hpp
+++ interface/minimal_digraph.hpp
@@ -6,10 +6,17 @@
 // This is a minimal digraph.  It's minimal, because it has no
 // properties associated with a vertex, and with an edge.  We only
 // know if an edge exists.
+//
+// We store the graph information in a matrix with boolean values,
+// because the graph can be dense.
+//
+// In such a graph we need a way to refer to a vertex, and it's going
+// to be an unsigned integer.  The vertex
+
 template <std::size_t N>
 using minimal_digraph = std::array<std::array<bool, N>, N>;
 
-/*
+
 template <std::size_t N>
 // We need the "auto &" return type (and not "auto") to prevent the
 // array decay into a pointer.
